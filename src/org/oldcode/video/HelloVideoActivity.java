21db302d42e1f1dc2773d3c2a293b9bd31c75938
paul wisehart <paul@oldcode.org>
2011-09-11 17:02:50 -0400

    activity 4.2

--- src/org/oldcode/video/HelloVideoActivity.java
+++ src/org/oldcode/video/HelloVideoActivity.java
@@ -2,15 +2,25 @@ package org.oldcode.video;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.VideoView;
 
 public class HelloVideoActivity extends Activity {
 
 	
-	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.main);
+
+        VideoView video = (VideoView) findViewById(R.id.video);
+        
+        // Load and start the movie 
+        //video.setVideoPath("/data/irene_bridge_bf.3gp" ); 
+        video.setVideoPath("/data/samplevideo.3gp" ); 
+        
+        video.start();
+        
     }
 
 
